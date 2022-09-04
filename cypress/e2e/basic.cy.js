eb091966401f225bae52b5a336baaaf0a110ae3c
caicodes <caicodes1@gmail.com>
2022-09-04 01:42:30 -0600

    cypress test fixed for new content

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,6 @@ describe('sample render test', () => {
   })
 
   it('displays the title text', () => {
-    cy.get('h2')
-    .contains('Essentials for a cold winter');
+    cy.get('h2').contains('MADEMOISELLE')
   })
-}) 
+})
