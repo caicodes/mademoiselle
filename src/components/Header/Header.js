655887e7307b4c6a2cec2307553015f40f131238
caicodes <caicodes1@gmail.com>
2022-09-03 22:49:53 -0600

    header updated for new branding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,82 +1,82 @@
-import React, { useState, useEffect, createRef } from 'react';
-import { Link, navigate } from 'gatsby';
+import React, { useState, useEffect, createRef } from 'react'
+import { Link, navigate } from 'gatsby'
 
-import { isAuth } from '../../helpers/general';
+import { isAuth } from '../../helpers/general'
 
-import AddNotification from '../AddNotification';
-import Brand from '../Brand';
-import Container from '../Container';
-import Config from '../../config.json';
-import Drawer from '../Drawer';
-import ExpandedMenu from '../ExpandedMenu';
-import FormInputField from '../FormInputField/FormInputField';
-import Icon from '../Icons/Icon';
-import MiniCart from '../MiniCart';
-import MobileNavigation from '../MobileNavigation';
-import * as styles from './Header.module.css';
+import AddNotification from '../AddNotification'
+import Brand from '../Brand'
+import Container from '../Container'
+import Config from '../../config.json'
+import Drawer from '../Drawer'
+import ExpandedMenu from '../ExpandedMenu'
+import FormInputField from '../FormInputField/FormInputField'
+import Icon from '../Icons/Icon'
+import MiniCart from '../MiniCart'
+import MobileNavigation from '../MobileNavigation'
+import * as styles from './Header.module.css'
 
 const Header = (prop) => {
-  const [showMiniCart, setShowMiniCart] = useState(false);
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const [showMenu, setShowMenu] = useState(true);
+  const [showMiniCart, setShowMiniCart] = useState(false)
+  const [mobileMenu, setMobileMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(true)
 
-  const [menu, setMenu] = useState();
-  const [activeMenu, setActiveMenu] = useState();
+  const [menu, setMenu] = useState()
+  const [activeMenu, setActiveMenu] = useState()
 
-  const [showSearch, setShowSearch] = useState(false);
-  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false)
+  const [search, setSearch] = useState('')
 
-  const searchRef = createRef();
-  const bannerMessage = 'Free shipping worldwide';
+  const searchRef = createRef()
+  const bannerMessage = 'Free shipping worldwide'
   const searchSuggestions = [
     'Oversize sweaters',
     'Lama Pajamas',
     'Candles Cinnamon',
-  ];
+  ]
 
   const handleHover = (navObject) => {
     if (navObject.category) {
-      setShowMenu(true);
-      setMenu(navObject.category);
-      setShowSearch(false);
+      setShowMenu(true)
+      setMenu(navObject.category)
+      setShowSearch(false)
     } else {
-      setMenu(undefined);
+      setMenu(undefined)
     }
-    setActiveMenu(navObject.menuLabel);
-  };
+    setActiveMenu(navObject.menuLabel)
+  }
 
   const handleSearch = (e) => {
-    e.preventDefault();
-    navigate(`/search?q=${search}`);
-    setShowSearch(false);
-  };
+    e.preventDefault()
+    navigate(`/search?q=${search}`)
+    setShowSearch(false)
+  }
 
   // disable active menu when show menu is hidden
   useEffect(() => {
-    if (showMenu === false) setActiveMenu(false);
-  }, [showMenu]);
+    if (showMenu === false) setActiveMenu(false)
+  }, [showMenu])
 
   // hide menu onscroll
   useEffect(() => {
     const onScroll = () => {
-      setShowMenu(false);
-      setShowSearch(false);
-      setActiveMenu(undefined);
-    };
-    window.removeEventListener('scroll', onScroll);
-    window.addEventListener('scroll', onScroll, { passive: true });
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+      setShowMenu(false)
+      setShowSearch(false)
+      setActiveMenu(undefined)
+    }
+    window.removeEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll, { passive: true })
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
 
   //listen for show search and delay trigger of focus due to CSS visiblity property
   useEffect(() => {
     if (showSearch === true) {
       setTimeout(() => {
-        searchRef.current.focus();
-      }, 250);
+        searchRef.current.focus()
+      }, 250)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showSearch]);
+  }, [showSearch])
 
   return (
     <div className={styles.root}>
@@ -90,9 +90,8 @@ const Header = (prop) => {
             <nav
               role={'presentation'}
               onMouseLeave={() => {
-                setShowMenu(false);
-              }}
-            >
+                setShowMenu(false)
+              }}>
               {Config.headerLinks.map((navObject) => (
                 <Link
                   key={navObject.menuLink}
@@ -100,8 +99,7 @@ const Header = (prop) => {
                   className={`${styles.navLink} ${
                     activeMenu === navObject.menuLabel ? styles.activeLink : ''
                   }`}
-                  to={navObject.menuLink}
-                >
+                  to={navObject.menuLink}>
                   {navObject.menuLabel}
                 </Link>
               ))}
@@ -110,11 +108,10 @@ const Header = (prop) => {
           <div
             role={'presentation'}
             onClick={() => {
-              setMobileMenu(!mobileMenu);
+              setMobileMenu(!mobileMenu)
               // setDepth(0);
             }}
-            className={styles.burgerIcon}
-          >
+            className={styles.burgerIcon}>
             <Icon symbol={`${mobileMenu === true ? 'cross' : 'burger'}`}></Icon>
           </div>
           <Brand />
@@ -123,33 +120,29 @@ const Header = (prop) => {
               aria-label="Search"
               className={`${styles.iconButton} ${styles.iconContainer}`}
               onClick={() => {
-                setShowSearch(!showSearch);
-              }}
-            >
+                setShowSearch(!showSearch)
+              }}>
               <Icon symbol={'search'}></Icon>
             </button>
             <Link
               aria-label="Favorites"
               href="/account/favorites"
-              className={`${styles.iconContainer} ${styles.hideOnMobile}`}
-            >
+              className={`${styles.iconContainer} ${styles.hideOnMobile}`}>
               <Icon symbol={'heart'}></Icon>
             </Link>
             <Link
               aria-label="Orders"
               href={isAuth() ? '/login' : '/account/orders/'}
-              className={`${styles.iconContainer} ${styles.hideOnMobile}`}
-            >
+              className={`${styles.iconContainer} ${styles.hideOnMobile}`}>
               <Icon symbol={'user'}></Icon>
             </Link>
             <button
               aria-label="Cart"
               className={`${styles.iconButton} ${styles.iconContainer} ${styles.bagIconContainer}`}
               onClick={() => {
-                setShowMiniCart(true);
-                setMobileMenu(false);
-              }}
-            >
+                setShowMiniCart(true)
+                setMobileMenu(false)
+              }}>
               <Icon symbol={'bag'}></Icon>
               <div className={styles.bagNotification}>
                 <span>1</span>
@@ -165,8 +158,7 @@ const Header = (prop) => {
         <div
           className={`${styles.searchContainer} ${
             showSearch === true ? styles.show : styles.hide
-          }`}
-        >
+          }`}>
           <h4>What are you looking for?</h4>
           <form className={styles.searchForm} onSubmit={(e) => handleSearch(e)}>
             <FormInputField
@@ -184,12 +176,11 @@ const Header = (prop) => {
               <p
                 role={'presentation'}
                 onClick={() => {
-                  setShowSearch(false);
-                  navigate(`/search?q=${suggestion}`);
+                  setShowSearch(false)
+                  navigate(`/search?q=${suggestion}`)
                 }}
                 key={index}
-                className={styles.suggestion}
-              >
+                className={styles.suggestion}>
                 {suggestion}
               </p>
             ))}
@@ -197,11 +188,10 @@ const Header = (prop) => {
           <div
             role={'presentation'}
             onClick={(e) => {
-              e.stopPropagation();
-              setShowSearch(false);
+              e.stopPropagation()
+              setShowSearch(false)
             }}
-            className={styles.backdrop}
-          ></div>
+            className={styles.backdrop}></div>
         </div>
       </Container>
 
@@ -212,8 +202,7 @@ const Header = (prop) => {
         onMouseEnter={() => setShowMenu(true)}
         className={`${styles.menuContainer} ${
           showMenu === true ? styles.show : ''
-        }`}
-      >
+        }`}>
         <Container size={'large'} spacing={'min'}>
           <ExpandedMenu menu={menu} />
         </Container>
@@ -231,13 +220,12 @@ const Header = (prop) => {
           top={'98px'}
           isReverse
           visible={mobileMenu}
-          close={() => setMobileMenu(false)}
-        >
+          close={() => setMobileMenu(false)}>
           <MobileNavigation close={() => setMobileMenu(false)} />
         </Drawer>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
